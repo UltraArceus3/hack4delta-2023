5c7711ccec72cd1a9d2cde05118737534a8708cf
Rany Kamel <rany.kamel47@gmail.com>
2023-09-16 23:02:06 -0400

    Added function to create clean dataset + plot fig tests
    Co-authored-by: Arav Parikh <aravp21@users.noreply.github.com>

--- plane_data.py
+++ plane_data.py
@@ -1,31 +1,67 @@
 import requests
 from datetime import datetime, timedelta
 import pandas as pd
+import os
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-# Get all files from this date
-start_date = datetime(2023, 9, 1)
-end_date = datetime(2023, 9, 2)
-for date in daterange(start_date, end_date):
-    date_str = date.strftime("%Y/%m/%d")
-    print(f"Date {date_str}" + 20 * "-")
-    base_url = f"https://samples.adsbexchange.com/traces/{date_str}"
-    index_resp = requests.get(base_url + "/index.json").json()
+# get all files from specified date
+def get_data(start_date, end_date):
+    for date in daterange(start_date, end_date):
+        date_str = date.strftime("%Y/%m/%d")
+        print(f"Date {date_str}" + 20 * "-")
+        base_url = f"https://samples.adsbexchange.com/traces/{date_str}"
+        index_resp = requests.get(base_url + "/index.json").json()
 
-    for i, trace_name in enumerate(index_resp['traces']):
-        print(f"Trace #{i + 1}: {trace_name} completed")
-        lsb_first_two_chars = trace_name[-2:]
-        trace_resp = requests.get(base_url + f"/{lsb_first_two_chars}/trace_full_{trace_name}.json")
+        for i, trace_name in enumerate(index_resp['traces']):
+            print(f"Trace #{i + 1}: {trace_name} completed")
+            lsb_first_two_chars = trace_name[-2:]
+            trace_resp = requests.get(base_url + f"/{lsb_first_two_chars}/trace_full_{trace_name}.json")
 
-        df = pd.DataFrame(trace_resp.json())
-        
-        file_date = date.strftime("%Y-%m-%d")
-        df.to_csv(f"data/plane_data/{trace_name}-{file_date}.csv")
+            df = pd.DataFrame(trace_resp.json())
+            
+            file_date = date.strftime("%Y-%m-%d")
+            df.to_csv(f"data/plane_data/{trace_name}-{file_date}.csv")
 
+# start_date = datetime(2023, 9, 1)
+# end_date = datetime(2023, 9, 2)
+# create_csvs(start_date, end_date)
 
+def create_dataset(start_date, bounding_box = {"lat": (22, 49), "long": (-129, -64)}, size_limit=-1):
+    l = []
+    i = 0
+    for file in os.listdir("./data/plane_data"):
+        if file.endswith(".csv"):
+            d = {}
+            df = pd.read_csv(f"./data/plane_data/{file}", converters={'trace': eval})
+            
+            id = df['icao']
+            lat = df['trace'].apply(lambda x: x[1])
+            lat_inrange = lat.apply(lambda x: x > bounding_box['lat'][0] and x < bounding_box['lat'][1])
+            lon = df['trace'].apply(lambda x: x[2])
+            lon_inrange = lon.apply(lambda x: x > bounding_box['long'][0] and x < bounding_box['long'][1])
+            
+            if sum(lat_inrange) == 0 or sum(lon_inrange) == 0:
+                continue
+            i += 1
+            
+            depart = start_date + timedelta(seconds=df.iloc[0]['trace'][0])
+            arrival = start_date + timedelta(seconds=df.iloc[-1]['trace'][0])
 
-    
-        
+            d['flight_id'] = id
+            d['latitude'] = lat
+            d['longitude'] = lon
+            d['departure'] = depart
+            d['arrival'] = arrival
+            l.append(d)
+
+        if size_limit > 0 and i >= size_limit:
+            break
+
+    df = pd.DataFrame(d)
+    df.to_csv("./data/dataset.csv")
+            
+start_date = datetime(2023, 9, 1, 0, 0, 0)
+create_dataset(start_date, size_limit=100)
--- plot.py
+++ plot.py
@@ -4,35 +4,101 @@ import plotly.graph_objects as go
 import os
 
 
-fig = go.Figure(go.Scattermapbox())
-i = 0
+fig = go.Figure(go.Densitymapbox())
 
 
 
-for file in os.listdir("./data/plane_data"):
+
+def flightmap():
+    fig = go.Figure(go.Scattergeo())
+    i = 0
+    for file in os.listdir("./data/plane_data"):
+        if file.endswith(".csv"):
+            i += 1
+            print(f"{i}", end="\r")
+            df = pd.read_csv(f"./data/plane_data/{file}", converters={'trace': eval})
+            fig.add_trace(go.Scattergeo(
+                lon = df['trace'].apply(lambda x: x[2] if x[2] > -129 and x[2] < -64 else None),
+                lat = df['trace'].apply(lambda x: x[1] if x[1] > 22 and x[1] < 49 else None),
+                mode = 'lines',
+                line = {'width': 1},
+                name = file
+            ))
+
+        if i > 100:
+            break
     
-    if file.endswith(".csv"):
-        df = pd.read_csv(f"./data/plane_data/{file}", converters={'trace': eval})
-        #print(df['trace'])
-        #print(list(df['trace']))
-        #df.update()
-        fig.add_trace(go.Scattermapbox(
-            lon = df['trace'].apply(lambda x: x[2] if x[2] > -129 and x[2] < -64 else None), #
-            lat = df['trace'].apply(lambda x: x[1] if x[1] > 22 and x[1] < 49 else None), #
-            mode = 'lines',
-            line = {'width': 1},
-            name = file
-        ))
-        
-    i += 1
+    fig.update_layout(mapbox = {'style': "stamen-terrain"})
+    fig.update_geos(scope = "usa",)
+    fig.show()
 
-    print(f"{i}", end = "\r")
+def heatmap():
+    fig = go.Figure(go.Densitymapbox())
+    i = 0
+    lon = []
+    lat = []
+    for file in os.listdir("./data/plane_data"):
+        if file.endswith(".csv"):
+            i += 1
+            print(f"{i}", end="\r")
+            df = pd.read_csv(f"./data/plane_data/{file}", converters={'trace': eval})
 
-    if i > 500:
-        break
+            lon += df['trace'].apply(lambda x: x[2] if x[2] > -129 and x[2] < -64 else None).tolist()
+            lat += df['trace'].apply(lambda x: x[1] if x[1] > 22 and x[1] < 49 else None).tolist()
 
+        if i > 1000:
+            break
 
+    fig.add_trace(go.Densitymapbox(
+        lon = lon,
+        lat = lat,
+        #mode = 'lines',
+        #line = {'width': 1},
+        #name = file
+    ))
 
+    fig.update_layout(mapbox = {'style': "stamen-terrain"})
+    fig.update_geos(scope = "usa",)
+    fig.show()
+
+def countymap():
+    from urllib.request import urlopen
+    import json
+    with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+        countiesjson = json.load(response)
+
+    
+    counties = pd.read_csv("./data/wildfire_risk_dataset.csv", converters={'STCOFIPS': str})
+    ca_counties = counties[counties['STATE'] == 'California']
+    fig = go.Figure(go.Choroplethmapbox(geojson=countiesjson,
+                    locations=ca_counties['STCOFIPS'], z=ca_counties['WFIR_RISKS'], colorscale="Viridis", zmin=min(counties['WFIR_RISKS']), zmax=100,
+                                    marker_opacity=0.5, marker_line_width=0))
+
+    i = 0
+
+    for file in os.listdir("./data/plane_data"):
+        if file.endswith(".csv"):
+            i += 1
+            print(f"{i}", end="\r")
+            df = pd.read_csv(f"./data/plane_data/{file}", converters={'trace': eval})
+            fig.add_trace(go.Scattermapbox(
+                lon = df['trace'].apply(lambda x: x[2] if x[2] > -129 and x[2] < -64 else None),
+                lat = df['trace'].apply(lambda x: x[1] if x[1] > 22 and x[1] < 49 else None),
+                mode = 'lines',
+                line = {'width': 1},
+                name = file
+            ))
+
+        if i > 1000:
+            break
+    
+    fig.update_layout(mapbox = {'style': "stamen-terrain"})
+    fig.update_layout(mapbox_style="carto-positron",
+                  mapbox_zoom=3, mapbox_center = {"lat": 37.0902, "lon": -95.7129})
+    fig.update_geos(scope = "usa",)
+    fig.show()
+
+countymap()
 
 # df = pd.read_json("data/trace_full_000000.json")
 # lat = df['trace'].apply(lambda x: x[1] if x[1] > 22 and x[1] < 49 else None)
@@ -50,12 +116,13 @@ for file in os.listdir("./data/plane_data"):
     marker = {'size': 10}),
     line = {'width': 45})
  """
+#fig.update_layout(
+#    mapbox = {'style': "stamen-terrain", 'zoom': 1})
 
-fig.update_layout(
-mapbox = {
-    'style': "stamen-terrain",
-    'zoom': 1})
 # fig.update_layout(
-#     geo = {'scope': 'north america'},)
+#     mapbox = {'style': "stamen-terrain", 
+#               })
 
-fig.show()
+# fig.update_geos(
+#     scope = "usa",)
+# fig.show()
